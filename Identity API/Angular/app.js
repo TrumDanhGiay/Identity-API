6f4c80c02f89d070b7d1880d846d405037968287
Trùm Đánh Giầy <Trm nh Giy>
2017-09-11 23:01:04 +0700

    Add project files.

---
+++ Identity API/Angular/app.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('app', []);
+
+app.service('apiCall', ['$http', function ($http) {
+    var result = {};
+
+    this.getApi = function (loginData) {
+        result = $http.post('/Token', loginData).success(function (data) {
+            result = (data);
+            console.log(data);
+        }).error(function () {
+            console.log('Loi vcd ra ');
+        });
+        return result;
+    }
+}]);
+
+app.controller('appController', ['$scope', function ($scope) {
+    var username = $scope.username;
+    var password = $scope.password;
+    console.log(username);
+    console.log(password);
+    $scope.suc = "thanh cong";
+    $scope.login = function () {
+        var loginData = {
+            grant_type: 'password',
+            username: "trang.duong39@yahoo.com.vn",
+            password: "25021996Aa"
+        };
+        var result = apiCall.getApi(loginData).success(function (data) {
+            console.log(data);
+            $scope.Data = data;
+        });
+    };
+}]);
+
---
+++ Identity API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Identity_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
